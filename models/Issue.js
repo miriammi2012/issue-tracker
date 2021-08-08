e63b6a13f3d57067a9f03941a04a6d1dedcc63fe
Afroz <zkindest5@gmail.com>
2021-08-08 17:29:57 +0530

    Create Issue.js

---
+++ models/Issue.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const issueSchema = new Schema({
+    issue_title: {type: String,required: true},
+    issue_text: {type: String,required: true},
+    created_by: {type: String,required: true},
+    assigned_to: {type: String,default: ''},
+    open: {type: Boolean,default: true},
+    status_text: {type: String,default: ''}
+
+},{
+  timestamps: {
+    createdAt: 'created_on',
+    updatedAt: 'updated_on'
+  }
+})
+const Issue = mongoose.model('issues',issueSchema);
+
+module.exports = Issue;
